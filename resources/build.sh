931e5ae1bc21ada2ceae4c1f8939d2217ed57202
Juraj Čarnogurský <juraj.carnogursky@student.tuke.sk>
2017-07-14 23:38:18 +0200

    just a commit

--- resources/build.sh
+++ resources/build.sh
@@ -10,11 +10,11 @@ fi
 
 rm -rf dist/ && mkdir -p dist/
 babel src --ignore __tests__ --out-dir dist/
-echo "Bundling graphiql.js..."
-browserify -g browserify-shim -s GraphiQL dist/index.js > graphiql.js
-echo "Bundling graphiql.min.js..."
-browserify -g browserify-shim -t uglifyify -s GraphiQL dist/index.js 2> /dev/null | uglifyjs -c > graphiql.min.js 2> /dev/null
-echo "Bundling graphiql.css..."
+echo "Bundling graphiql-custom-headers.js..."
+browserify -g browserify-shim -s GraphiQL dist/index.js > graphiql-custom-headers.js
+echo "Bundling graphiql-custom-headers.min.js..."
+browserify -g browserify-shim -t uglifyify -s GraphiQL dist/index.js 2> /dev/null | uglifyjs -c > graphiql-custom-headers.min.js 2> /dev/null
+echo "Bundling graphiql-custom-headers.css..."
 postcss --no-map --use autoprefixer -d dist/ css/*.css
-cat dist/*.css > graphiql.css
+cat dist/*.css > graphiql-custom-headers.css
 echo "Done"
